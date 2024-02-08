f6ab8cf758999265ed5417b8c7470c13802f70f9
Armory <armin.tovar44@gmail.com>
2024-02-08 14:52:59 -0600

    attachment feature added

--- cogs/misc.py
+++ cogs/misc.py
@@ -18,6 +18,10 @@ class Misc(commands.Cog):
             user = await self.bot.fetch_user(payload.user_id)
         embed=discord.Embed(title="Mensaje guardado", description=message.content, color=0xFF5733)
         embed.set_author(name=message.author.name, icon_url=message.author.avatar)
+        if message.attachments:
+            embed.add_field(name="Adjunto", value=message.attachments[0].url, inline=False)
+            if message.attachments[0].content_type.split('/')[0] == 'image':
+                embed.set_image(url=message.attachments[0].url)
         await user.send(content=message.jump_url, embed=embed)
 
     @commands.command()
