b4640215250ba4202e096184943a88b36f7107e8
Armory <armin.tovar44@gmail.com>
2024-02-08 18:40:56 -0600

    reaction fixed

--- cogs/misc.py
+++ cogs/misc.py
@@ -16,13 +16,14 @@ class Misc(commands.Cog):
         user = self.bot.get_user(payload.user_id)
         if not user:
             user = await self.bot.fetch_user(payload.user_id)
-        embed=discord.Embed(title="Mensaje guardado", description=message.content, color=0xFF5733)
-        embed.set_author(name=message.author.name, icon_url=message.author.avatar)
-        if message.attachments:
-            embed.add_field(name="Adjunto", value=message.attachments[0].url, inline=False)
-            if message.attachments[0].content_type.split('/')[0] == 'image':
-                embed.set_image(url=message.attachments[0].url)
-        await user.send(content=message.jump_url, embed=embed)
+        if payload.emoji.name == '🔖':
+            embed=discord.Embed(title="Mensaje guardado", description=message.content, color=0xFF5733)
+            embed.set_author(name=message.author.name, icon_url=message.author.avatar)
+            if message.attachments:
+                embed.add_field(name="Adjunto", value=message.attachments[0].url, inline=False)
+                if message.attachments[0].content_type.split('/')[0] == 'image':
+                    embed.set_image(url=message.attachments[0].url)
+            await user.send(content=message.jump_url, embed=embed)
 
     @commands.command()
     async def ping(self, ctx):
